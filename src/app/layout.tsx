53b46d2cbe8160e96a7078b09f568c48d3edd542
Gonçalo Passos <dev@diogopassos.pt>
2023-11-21 23:14:08 +0000

    format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,28 @@ const font = League_Spartan({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Diogo Passos | Software Engineer',
-  description: 'Diogo Passos is an experienced software engineer with a passion for developing innovative solutions. Browse the site to learn more about his projects, skills, and professional experiences.',
-  keywords: ['software engineer', 'software', 'developer', 'personal', 'website', 'software development', 'frontend', 'innovative', 'solutions', 'digital channels', 'skills', 'experience']
+  description:
+    'Diogo Passos is an experienced software engineer with a passion for developing innovative solutions. Browse the site to learn more about his projects, skills, and professional experiences.',
+  keywords: [
+    'software engineer',
+    'software',
+    'developer',
+    'personal',
+    'website',
+    'software development',
+    'frontend',
+    'innovative',
+    'solutions',
+    'digital channels',
+    'skills',
+    'experience',
+  ],
 };
 
 export const viewport: Viewport = {
   initialScale: 1,
-  width: "device-width"
-}
+  width: 'device-width',
+};
 
 export default function RootLayout({
   children,
