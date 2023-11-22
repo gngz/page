25ddc4539cb077c83da9c9895451638ad80dd32c
Gonçalo Passos <dev@diogopassos.pt>
2023-11-22 18:22:43 +0000

    Add SocialNetwork api implementation

---
+++ src/services/cms-api/apis/social-networks.ts
@@ -0,0 +1,34 @@
+import { CMSApi } from '../api';
+import {
+  SocialNetworkModel,
+  SocialNetworkSchema,
+} from '../models/social-network';
+
+type SocialNetworksResponse = {
+  social_networks: SocialNetworkModel[];
+};
+
+export async function getSocialNetworks() {
+  const query = `
+      {
+        social_networks(
+          filter: {
+            status: {_eq: "published"}
+          }
+        ) {
+          code,
+          name,
+          url
+        }
+      }
+  `;
+
+  const collection = 'social_networks';
+  const result = await CMSApi.getInstance().query<SocialNetworksResponse>(
+    query,
+    undefined,
+    ['all', collection],
+  );
+
+  return SocialNetworkSchema.array().parse(result.social_networks);
+}
---
+++ src/services/cms-api/index.ts
@@ -0,0 +1,1 @@
+export * from './apis/social-networks';
