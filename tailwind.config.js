0b00f96e57fd5338ad30a7dc48d1ad7ac333e2e4
Gonçalo Passos <mail@diogopassos.pt>
2024-01-23 18:06:21 +0000

    feat: add blink cursor animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -66,10 +66,16 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        "blink-cursor": {
+          "0%": { opacity: 0 },
+          "50%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        "blink-cursor": "blink-cursor 1s step-end infinite"
       },
     },
   },
