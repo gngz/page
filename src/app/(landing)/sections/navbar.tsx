b4cfff1c1aabc6c1365d7a2b06dfa4695d1254be
Gonçalo Passos <mail@diogopassos.pt>
2024-01-23 19:03:50 +0000

    refactor: update navbar

--- src/app/(landing)/sections/navbar.tsx
+++ src/app/(landing)/sections/navbar.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export function Navbar() {
   return (
     <nav>
-      <div className='container flex justify-between'>
-        <div>LOGO</div>
+      <div className='container flex justify-between py-2'>
+        <div>
+          <Image
+            src={'/logo.svg'}
+            alt={'Diogo Passos'}
+            width={'224'}
+            height={'30'}
+          ></Image>
+        </div>
         <div className='flex gap-2'>
           <Link href={'#'}>Home</Link>
           <Link href={'#'}>About</Link>
