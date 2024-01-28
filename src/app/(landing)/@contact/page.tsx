09746e0001f3bb6c9beaa45a8e7a4647427ed009
Gonçalo Passos <mail@diogopassos.pt>
2024-01-28 17:06:22 +0000

    feat: add contact parallel route

---
+++ src/app/(landing)/@contact/page.tsx
@@ -0,0 +1,9 @@
+import { SectionLayout } from '../components/section-layout';
+
+export default function Contact() {
+  return (
+    <SectionLayout title='Contact' subtitle='Feel free to contact-me.'>
+      Hello World
+    </SectionLayout>
+  );
+}
