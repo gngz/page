700b2d512b5631dd1d08dfc672a0c739b3dd9306
Gonçalo Passos <mail@diogopassos.pt>
2024-01-28 17:05:54 +0000

    feat: add section layout

---
+++ src/app/(landing)/components/section-layout/section-layout.tsx
@@ -0,0 +1,24 @@
+import { Heading } from '@/components/ui/heading';
+import { SectionLayoutProps } from './types';
+
+export function SectionLayout({
+  title,
+  subtitle,
+  children,
+}: Readonly<SectionLayoutProps>) {
+  return (
+    <section>
+      <div className='container'>
+        <Heading as='h1' className='text-center text-5xl font-medium'>
+          {title}
+        </Heading>
+        {subtitle && (
+          <Heading as='h2' className='text-center text-2xl'>
+            {subtitle}
+          </Heading>
+        )}
+        {children}
+      </div>
+    </section>
+  );
+}
