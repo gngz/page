077dd7b0c29f27fcc5696ad19bab9419783eda23
Gonçalo Passos <mail@diogopassos.pt>
2024-01-31 17:25:39 +0000

    feat: add fallback image to post

--- src/app/(landing)/@blog/components/post.tsx
+++ src/app/(landing)/@blog/components/post.tsx
@@ -5,7 +5,9 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { imageFallback } from '@/lib/post-image-fallback';
 import { PostModel } from '@/services/cms-api/apis/blog/models/post';
+import Image from 'next/image';
 import Link from 'next/link';
 
 type Props = {
@@ -20,9 +22,18 @@ const formatDate = (date: Date) => {
 };
 
 export default function BlogPost({ post }: Readonly<Props>) {
+  const fallbackImageSrc = imageFallback(post.title, post.user_created.name);
   return (
     <Link href={`/blog/${post.slug}`}>
       <Card key={post.slug}>
+        <Image
+          src={fallbackImageSrc}
+          alt={post.title}
+          width={0}
+          height={0}
+          sizes='100vw'
+          style={{ width: '100%', height: 'auto' }}
+        />
         <CardHeader>
           <CardTitle>{post.title}</CardTitle>
         </CardHeader>
