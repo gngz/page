310897a9421b452842bcdb3124263ebb94753e86
Gonçalo Passos <mail@diogopassos.pt>
2024-02-02 16:25:08 +0000

    refactor: add code block styling

--- src/components/syntax-highlight/index.tsx
+++ src/components/syntax-highlight/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import hljs from 'highlight.js';
-import 'highlight.js/styles/github.css';
 import { PropsWithChildren, useEffect } from 'react';
 
 export function SyntaxHighlight({ children }: Readonly<PropsWithChildren>) {
--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,16 +1,18 @@
-import markdownit from "markdown-it/lib";
-import hljs from 'highlight.js'
+import hljs from 'highlight.js';
+import markdownit from 'markdown-it/lib';
 
 export function renderMarkdown(markdown: string) {
     const md = markdownit({
         highlight: function (str, lang) {
             if (lang && hljs.getLanguage(lang)) {
                 try {
-                    return hljs.highlight(str, { language: lang }).value;
+                    return (
+                        `<pre><div class="codelang">${hljs.getLanguage(lang)?.name}</div><code class="hljs">${hljs.highlight(str, { language: lang }).value}</code></pre>`
+                    );
                 } catch (__) { }
             }
             return '';
-        }
+        },
     });
     return md.render(markdown);
 }
