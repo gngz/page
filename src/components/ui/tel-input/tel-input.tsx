7f69ea47a2c843db3da719a45a036eaa78dcba26
Gonçalo Passos <mail@diogopassos.pt>
2024-02-05 20:45:17 +0000

    refactor: update tel input

--- src/components/ui/tel-input/tel-input.tsx
+++ src/components/ui/tel-input/tel-input.tsx
@@ -8,12 +8,12 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 import * as React from 'react';
 import { useState } from 'react';
 import { Input } from '../input';
 import CountryCodes from './country-codes.json';
 import { getNumberPlaceholder } from './utils';
-
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   defaultCountry?: string;
@@ -35,7 +35,7 @@ const TelInput = React.forwardRef<HTMLInputElement, InputProps>(
             className='w-auto rounded-r-none border-r-0 focus:!ring-transparent'
             tabIndex={-1}
           >
-            <SelectValue className='truncate text-ellipsis border-r-0' />
+            <SelectValue className='truncate text-ellipsis border-r-0 box-border' />
           </SelectTrigger>
           <SelectContent>
             {CountryCodes.map((country) => (
@@ -44,14 +44,16 @@ const TelInput = React.forwardRef<HTMLInputElement, InputProps>(
                 value={country.code}
                 className='px-2'
               >
-                <div className='flex items-center gap-2 truncate text-ellipsis'>
-                  <picture className='w-4 h-4 flex justify-center items-center'>
-                    <img
-                      alt={country.name}
-                      src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${country.code}.svg`}
-                    />
-                  </picture>
-                  {country.name}
+                <div className='flex flex-shrink-0 items-center gap-3'>
+                  <Image
+                    src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${country.code}.svg`}
+                    alt={country.name}
+                    title={country.name}
+                    className='w-4 h-4'
+                    height={0}
+                    width={0}
+                  />
+                  <div className='mr-2 shrink-0'>{country.name}</div>
                 </div>
               </SelectItem>
             ))}
