2d9019e11c3861187600419a2411839fddbd511e
Gonçalo Passos <mail@diogopassos.pt>
2024-02-05 16:46:10 +0000

    feat: add contact form layout

--- src/app/(landing)/@contact/page.tsx
+++ src/app/(landing)/@contact/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
 import { SectionLayout } from '../components/section-layout';
 
 export default function Contact() {
@@ -7,7 +11,35 @@ export default function Contact() {
       subtitle='Feel free to contact-me.'
       id='contact'
     >
-      <div className='grid grid-cols-2'></div>
+      <form action=''>
+        <div className='grid md:grid-cols-2 gap-3 md:w-4/6 md:mx-auto'>
+          <div className='md:col-span-2'>
+            <Label htmlFor='name'>Name</Label>
+            <Input id='name' type='text' />
+          </div>
+          <div>
+            <Label htmlFor='email'>E-mail</Label>
+            <Input id='email' type='email' />
+          </div>
+          <div>
+            <Label htmlFor='phone'>Phone Number</Label>
+            <Input id='phone' type='text' />
+          </div>
+          <div className='md:col-span-2'>
+            <Label htmlFor='subject'>Subject</Label>
+            <Input id='subject' type='text' />
+          </div>
+          <div className='md:col-span-2'>
+            <Label htmlFor='message'>Message</Label>
+            <Textarea id='message' />
+          </div>
+          <div className='flex justify-end md:col-span-2'>
+            <Button type='submit' className='self-end'>
+              Send
+            </Button>
+          </div>
+        </div>
+      </form>
     </SectionLayout>
   );
 }
