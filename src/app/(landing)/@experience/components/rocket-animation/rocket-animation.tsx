771f6d123450f6987ad29d69f33fb987c14333d4
Gonçalo Passos <mail@diogopassos.pt>
2024-02-09 13:06:42 +0000

    refactor: improve rocket animation to webp

--- src/app/(landing)/@experience/components/rocket-animation/rocket-animation.tsx
+++ src/app/(landing)/@experience/components/rocket-animation/rocket-animation.tsx
@@ -1,17 +1,14 @@
 import Image from 'next/image';
 export function RocketAnimation() {
   return (
-    <picture>
-      <source srcSet='/rocket.webp' type='image/webp' />
-      <Image
-        src='/rocket.gif'
-        width={40}
-        height={40}
-        className='p-1 pointer-events-none'
-        quality={100}
-        alt='🚀'
-        priority
-      />
-    </picture>
+    <Image
+      src='/rocket.webp'
+      width={40}
+      height={40}
+      className='p-1 pointer-events-none'
+      quality={100}
+      alt='🚀'
+      priority
+    />
   );
 }
