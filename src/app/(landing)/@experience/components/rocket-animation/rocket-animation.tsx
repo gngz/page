04bf372b304109b97ec5bcb357d57cc2c7259b34
Gonçalo Passos <mail@diogopassos.pt>
2024-02-09 10:58:29 +0000

    refactor: add lottie animation

---
+++ src/app/(landing)/@experience/components/rocket-animation/rocket-animation.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import Lottie from 'lottie-react';
+import rocketAnimation from './animation.json';
+
+export function RocketAnimation() {
+  return <Lottie animationData={rocketAnimation} />;
+}
