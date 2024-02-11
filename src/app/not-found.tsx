b8706e782dd94e48bd0dd1d7f2e9087489fd3dbd
Gonçalo Passos <mail@diogopassos.pt>
2024-02-11 19:00:50 +0000

    feat: add 404 not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,34 @@
+import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
+import Image from 'next/image';
+import Link from 'next/link';
+export default function NotFound() {
+  return (
+    <main className=' bg-slate-50'>
+      <div className='container text-center pt-8 min-h-[calc(100svh-327px)]'>
+        <Image
+          src={'/404.svg'}
+          alt='404'
+          width={300}
+          height={300}
+          className='mx-auto py-6'
+        />
+
+        <Heading className='text-4xl font-bold'>
+          <Text as='span' className='text-red-400 text-5xl'>
+            Whoops!
+          </Text>{' '}
+          Page not found!
+        </Heading>
+        <Heading className='text-2xl font-light mt-4 max-w-screen-sm mx-auto'>
+          I can&apos;t find the page that are you looking for, please try again
+          later or{' '}
+          <Link href='/#contact' className='underline underline-offset-2'>
+            contact me
+          </Link>
+          .
+        </Heading>
+      </div>
+    </main>
+  );
+}
