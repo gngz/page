661bf9d278762934e2112ac5f14bc68cdbaff197
Gonçalo Passos <mail@diogopassos.pt>
2024-02-14 23:25:34 +0000

    chore: format code

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,23 +1,23 @@
 import { getSeoData } from '@/services/cms-api';
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from 'next';
 
 export default async function manifest(): Promise<MetadataRoute.Manifest> {
-    const seo = await getSeoData();
+  const seo = await getSeoData();
 
-    return {
-        name: seo.title ?? 'Diogo Passos | Software Developer',
-        short_name: seo.title ?? 'Diogo Passos',
-        description: seo.description,
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#fff',
-        theme_color: '#F1F5F9',
-        icons: [
-            {
-                src: '/favicon.ico',
-                sizes: 'any',
-                type: 'image/x-icon',
-            },
-        ],
-    }
+  return {
+    name: seo.title ?? 'Diogo Passos | Software Developer',
+    short_name: seo.title ?? 'Diogo Passos',
+    description: seo.description,
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#F1F5F9',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  };
 }
