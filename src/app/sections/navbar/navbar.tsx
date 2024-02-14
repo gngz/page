67c7ef862997670b015b166c89ff27caf3f37fa1
Gonçalo Passos <mail@diogopassos.pt>
2024-02-14 12:22:03 +0000

    refactor: navbar

--- src/app/sections/navbar/navbar.tsx
+++ src/app/sections/navbar/navbar.tsx
@@ -8,7 +8,7 @@ export function Navbar() {
     <nav className='bg-slate-100 py-4 sticky top-0 z-50'>
       <div className='container flex items-center justify-between'>
         <div>
-          <Link href={'/'}>
+          <Link href={'/#top'}>
             <Image
               src={'/logo.svg'}
               alt={'Diogo Passos'}
