e3f6ce58fe319222b5f37b3ef95dc4a3f811d7f0
Gonçalo Passos <mail@diogopassos.pt>
2024-02-15 11:04:40 +0000

    refactor: add robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from 'next'
+
+const baseURL = process.env.WEB_URL ?? 'https://diogopassos.pt';
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: [
+            {
+                userAgent: '*',
+                allow: ['/'],
+            },
+        ],
+        sitemap: `${baseURL}/sitemap.xml`,
+    }
+}
