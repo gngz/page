65d3342d7d8d0b35e2c4c5c92edf388b6163206d
Gonçalo Passos <mail@diogopassos.pt>
2024-04-09 21:16:35 +0100

    chore: update folder structure and init monorepo

---
+++ apps/frontend/src/app/(landing)/@experience/components/badge-hr.tsx
@@ -0,0 +1,18 @@
+import { Badge } from '../../../../components/ui/badge';
+import { PropsWithChildren } from 'react';
+
+export function BadgeHR({ children }: Readonly<PropsWithChildren>) {
+  return (
+    <div className='relative my-6 select-none'>
+      <hr className=' absolute left-0 right-0 top-1/2 h-px -translate-y-1/2 border-0 bg-gray-200 dark:bg-gray-700' />
+      <Badge
+        className='relative left-1/2 top-0 inline-flex -translate-x-1/2 items-center bg-white'
+        variant={'outline'}
+      >
+        <span className='h-[10px] font-normal leading-none text-slate-800'>
+          {children}
+        </span>
+      </Badge>
+    </div>
+  );
+}
---
+++ apps/frontend/src/app/(landing)/@skills/page.tsx
@@ -0,0 +1,67 @@
+import { Heading } from '../../../components/ui/heading';
+import { PropsWithChildren } from 'react';
+import { SectionLayout } from '../components/section-layout';
+import { Skill } from './components/skill';
+
+const SkillsList = ({ children }: Readonly<PropsWithChildren>) => {
+  return (
+    <div className='mb-6 grid grid-cols-2 gap-4 sm:grid-cols-4 lg:flex lg:flex-wrap lg:justify-center'>
+      {children}
+    </div>
+  );
+};
+
+export default function Skills() {
+  return (
+    <SectionLayout
+      title='Skills and Tecnologies'
+      subtitle='Here are some of my skills and my prefered technologies.'
+      id='skills'
+    >
+      <Heading as='h3' className='mb-6 text-center text-2xl font-light'>
+        Software Development
+      </Heading>
+      <SkillsList>
+        <Skill skill='nextjs' />
+        <Skill skill='react' />
+        <Skill skill='typescript' />
+        <Skill skill='javascript' />
+        <Skill skill='php' />
+        <Skill skill='csharp' />
+        <Skill skill='golang' />
+        <Skill skill='dart' />
+        <Skill skill='sass' />
+        <Skill skill='tailwind' />
+        <Skill skill='node' />
+        <Skill skill='bun' />
+        <Skill skill='prisma' />
+      </SkillsList>
+      <Heading as='h3' className='mb-6 text-center text-2xl font-light'>
+        Systems & DevOps
+      </Heading>
+      <SkillsList>
+        <Skill skill='linux' />
+        <Skill skill='ansible' />
+        <Skill skill='docker' />
+        <Skill skill='aws' />
+        <Skill skill='vercel' />
+        <Skill skill='cloudflare' />
+        <Skill skill='postgres' />
+        <Skill skill='mysql' />
+        <Skill skill='nginx' />
+        <Skill skill='caddy' />
+      </SkillsList>
+      <Heading as='h3' className='mb-6 text-center text-2xl font-light'>
+        Tools
+      </Heading>
+      <SkillsList>
+        <Skill skill='vscode' />
+        <Skill skill='postman' />
+        <Skill skill='figma' />
+        <Skill skill='git' />
+        <Skill skill='github' />
+        <Skill skill='gitlab' />
+      </SkillsList>
+    </SectionLayout>
+  );
+}
