d0f284c2bed524c233f8ad3a2b9d7237f91fa475
Gonçalo Passos <mail@diogopassos.pt>
2024-04-11 22:58:08 +0100

    feat: add tooltip atom

--- apps/frontend/src/components/atoms/index.ts
+++ apps/frontend/src/components/atoms/index.ts
@@ -3,3 +3,4 @@ export { default as Checkbox } from './checkbox/checkbox';
 export { default as Input } from './input/input';
 export { default as Label } from './label/label';
 export { default as TextArea } from './textarea/textarea';
+export { default as Tooltip } from './tooltip/tooltip';
