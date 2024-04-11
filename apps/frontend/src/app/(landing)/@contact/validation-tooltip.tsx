f434bd51e2e903755b486fa8d97323876b84043a
Gonçalo Passos <mail@diogopassos.pt>
2024-04-11 22:58:31 +0100

    refactor: update validation tooltip

--- apps/frontend/src/app/(landing)/@contact/validation-tooltip.tsx
+++ apps/frontend/src/app/(landing)/@contact/validation-tooltip.tsx
@@ -1,10 +1,5 @@
+import { Tooltip } from '@/components/atoms';
 import { FaTriangleExclamation } from 'react-icons/fa6';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '../../../components/old/tooltip';
 
 type Props = {
   validationMessage: string;
@@ -12,15 +7,10 @@ type Props = {
 
 export function ValidationTooltip({ validationMessage }: Readonly<Props>) {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <FaTriangleExclamation className='ml-1 text-red-500' />
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>{validationMessage}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Tooltip content={validationMessage}>
+      <div className='ml-1 flex items-center justify-center'>
+        <FaTriangleExclamation className=' text-red-500' />
+      </div>
+    </Tooltip>
   );
 }
