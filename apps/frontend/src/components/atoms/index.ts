000242ee64ba9004190277b642ebb8b297b4fa71
Gonçalo Passos <mail@diogopassos.pt>
2024-04-13 16:48:20 +0100

    feat: add Select Atom

--- apps/frontend/src/components/atoms/index.ts
+++ apps/frontend/src/components/atoms/index.ts
@@ -1,7 +1,9 @@
+export { Flex } from '@radix-ui/themes';
 export { default as Badge } from './badge/badge';
 export { default as Button } from './button/button';
 export { default as Checkbox } from './checkbox/checkbox';
 export { default as Input } from './input/input';
 export { default as Label } from './label/label';
+export { default as Select } from './select/select';
 export { default as TextArea } from './textarea/textarea';
 export { default as Tooltip } from './tooltip/tooltip';
---
+++ apps/frontend/src/components/atoms/select/types.ts
@@ -0,0 +1,19 @@
+import { Select as RadixSelect } from '@radix-ui/themes';
+import { ComponentProps } from 'react';
+
+type SelectVariantType = ComponentProps<typeof RadixSelect.Trigger>['variant'];
+type SelectSizeType = ComponentProps<typeof RadixSelect.Root>['size'];
+export type SelectItem = {
+  label: string;
+  value: string;
+  icon?: JSX.Element;
+};
+
+export type SelectProps = {
+  initialValue?: string;
+  placeholder?: string;
+  size?: SelectSizeType;
+  variant?: SelectVariantType;
+  items: SelectItem[];
+  onChange?: (value: string) => void;
+};
